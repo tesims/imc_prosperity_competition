e21191b631770589e57aca711644e77b5c3e3912
tesims <tesims@umich.edu>
2025-05-05 02:23:25 -0400

    fix: Export TimeSeriesTrainer and TimeGAN classes

--- src/synthetic_data/__init__.py
+++ src/synthetic_data/__init__.py
@@ -1,3 +1,5 @@
 from .generator import SyntheticDataGenerator
+from .train import TimeSeriesTrainer
+from .models import TimeGAN
 
-__all__ = ['SyntheticDataGenerator']
+__all__ = ['SyntheticDataGenerator', 'TimeSeriesTrainer', 'TimeGAN']
